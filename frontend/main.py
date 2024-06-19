22826e0673fc0fa0e90c28c09ad7bd8e7b2c5f49
Travis Harry <travisdharry@gmail.com>
2024-06-19 10:46:38 -0500

    completed compare_franchise visualization

--- frontend/main.py
+++ frontend/main.py
@@ -1,8 +1,12 @@
 # Import dependencies
+import json
 import requests
 import pandas as pd
 from flask import Flask, redirect, request, url_for, render_template, session
 from sleeperpy import User, Leagues
+import plotly
+import plotly.express as px
+import plotly.graph_objects as go
 
 # Initialize app
 app = Flask(__name__)
@@ -72,15 +76,71 @@ def franchise_comparison():
     backendapi_franchisecomparison_url = f'https://ygn5at2pt5zgycacip4t6kvrey0ayfrl.lambda-url.us-east-2.on.aws//franchise_comparison/{user_id}/{league_id}'
     response = requests.get(backendapi_franchisecomparison_url).json()
     # Parse the data
-    franchise_comparisons = pd.DataFrame(response)
-    # Aggregate the data
-    # waiver_wire = waivers.groupby(['id_sleeper', 'team_name', 'full_name', 'age', 'team', 'number', 'position'])['predicted_fantasy_points'].sum().to_frame().reset_index()
-    # waiver_wire['predicted_fantasy_points'] = waiver_wire['predicted_fantasy_points'].round(2)
-    # waiver_wire = waiver_wire.drop(columns=['id_sleeper'])
-    # waiver_wire = waiver_wire.sort_values('predicted_fantasy_points', ascending=False, ignore_index=True)
-    franchise_comparisons = franchise_comparisons.loc[franchise_comparisons['full_name']=="Jalen Hurts"]
-    return render_template("waiverWire.html", tables=[franchise_comparisons.to_html(classes='data')], titles=franchise_comparisons.columns.values)
+    backend_response = pd.DataFrame(response)
+    # Set datatypes
+    backend_response['id_sleeper'] = backend_response['id_sleeper'].astype(str)
+    backend_response['team_name'] = backend_response['team_name'].astype(str)
 
+    # Aggregate the predicted points
+    player_sums = backend_response.groupby(['id_sleeper'])['predicted_fantasy_points'].sum().to_frame().reset_index()
+    total_franchise_values = backend_response.groupby('team_name')['predicted_fantasy_points'].sum().round(0).to_frame().reset_index()
+    # Merge back in the player info
+    player_info = backend_response.drop(columns=['predicted_fantasy_points', 'week_of_season']).drop_duplicates(ignore_index=True)
+    franchise_comparisons = player_sums.merge(player_info, on='id_sleeper', how='left')
+    # Visualize the data
+    fig = px.bar(franchise_comparisons, 
+                x="team_name", 
+                y='predicted_fantasy_points', 
+                color="position", 
+                text='full_name', 
+                color_discrete_map={ 
+                    "QB": "#9CCFD6", 
+                    "RB": "#DEB2AF", 
+                    "WR": "#97BFF0", 
+                    "TE": "#CEE1B2", 
+                    "K": "#F0C397", 
+                    "DEF": "#F0DB97"}, 
+                category_orders={
+                    "position": ["QB", "RB", "WR", "TE", "K", "DEF"]},
+                hover_name="full_name",
+                hover_data={
+                    'predicted_fantasy_points':True,
+                    'full_name':False, 'position':False, 'team_name':False
+                    },
+                labels={
+                    "team_name":"Franchise",
+                    'predicted_fantasy_points':"Predicted Points",
+                }
+                )
+    fig.update_traces(
+        textposition='inside',
+    )
+    fig.update_yaxes(
+        visible=False
+    )
+    fig.add_trace(go.Scatter(
+                x=total_franchise_values['team_name'], 
+                y=total_franchise_values['predicted_fantasy_points'],
+                text=total_franchise_values['predicted_fantasy_points'],
+                mode='text',
+                textposition='top center',
+                textfont=dict(
+                    size=12,
+                ),
+                showlegend=False
+            ))
+    fig.update_layout(
+                barmode='stack', 
+                xaxis={'categoryorder':'total descending'},
+                plot_bgcolor='rgba(0,0,0,0)',
+                title="Franchise Comparison",
+                font_family="Input Serif",
+                showlegend=False,
+                xaxis_title=None
+                )
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return render_template('compareFranchises.html', graphJSON=graphJSON)
+    #return render_template("waiverWire.html", tables=[backend_response.to_html(classes='data')], titles=backend_response.columns.values)
 
 if __name__ == "__main__":
 	app.run()
